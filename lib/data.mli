d5d0d1ab82c71e4d79a76f38dc1e4ea10a389ac1
Raul <raulrufino0@gmail.com>
2021-04-25 09:26:55 -0300

    docs([data|run].ml): Fix misspelled phrases in the docs of these files.

--- lib/data.mli
+++ lib/data.mli
@@ -4,7 +4,7 @@ type ('symbol, 'state) transition =
 (**
 This is a function that represent a transition of a turing machine. 
 
-The transition function can be not defined to all pairs or state-symbol,
+The transition function can be not defined to all pairs of state-symbol,
 to represent it, is used the type [ 'a option ].
 So, this function can return [ None ],
 to say that the given state and symbol isn't accepted. 
@@ -26,10 +26,10 @@ The last element is the function of transition.
 
 type ('symbol, 'state) turingMachineID = 'symbol Tape.tape * 'state
 (**
-This type give the instantaneous description (ID) 
+This type gives the instantaneous description (ID) 
 of a turing machine.
 
-The first element is a tape, which include the symbol in head.
+The first element is a tape, which includes the symbol in the head.
 The last element is the internal state in a given moment.
  *)
 
