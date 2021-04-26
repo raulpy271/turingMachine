16b411912a9a4a3a079f314896d0b3463484f594
Raul <raulrufino0@gmail.com>
2021-04-26 07:54:49 -0300

    fix(tape): Change internal values to snake_case

--- lib/tape.ml
+++ lib/tape.ml
@@ -4,20 +4,21 @@ type 'symbol tape = 'symbol list * 'symbol * 'symbol list;;
 type shift_side = Left | Right;;
 
 let left_swift empty tp = match tp with
-  | ([], head, rightSide) -> ([], empty, head :: rightSide)
-  | (leftSide, head, rightSide) -> 
-      let leftSwiftWithLastInFront = Utils.putLastInFront leftSide in
-      let lastOfLeftSide = List.hd leftSwiftWithLastInFront in 
-      let newLeftSide = List.tl leftSwiftWithLastInFront in
-      (newLeftSide, lastOfLeftSide, head :: rightSide)
+  | ([], head, right_side) -> ([], empty, head :: right_side)
+  | (left_side, head, right_side) -> 
+      let left_side_with_last_in_front = 
+        Utils.put_last_in_front left_side in
+      let last_of_left_side = List.hd left_side_with_last_in_front in 
+      let new_left_side = List.tl left_side_with_last_in_front in
+      (new_left_side, last_of_left_side, head :: right_side)
 ;;
 
 let right_shift empty tp = match tp with
-  | (leftSide, head, []) -> (leftSide @ [head], empty, [])
-  | (leftSide, head, rightSide) -> 
-      let firstOfRightSide = List.hd rightSide in 
-      let newRightSide = List.tl rightSide in
-      (leftSide @ [head], firstOfRightSide, newRightSide)
+  | (left_side, head, []) -> (left_side @ [head], empty, [])
+  | (left_side, head, right_side) -> 
+      let first_of_right_side = List.hd right_side in 
+      let new_right_side = List.tl right_side in
+      (left_side @ [head], first_of_right_side, new_right_side)
 ;;
 
 let shift_tape side empty tp = match side with
