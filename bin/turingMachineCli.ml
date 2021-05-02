59520e102e6a2c5aea054a489c80124d7d71759a
Raul <raulrufino0@gmail.com>
2021-05-02 08:28:47 -0300

    feat(bin): Run a machine in the main script

--- bin/turingMachineCli.ml
+++ bin/turingMachineCli.ml
@@ -1,9 +1,29 @@
-open TuringMachine
+open TuringMachine;;
+open Examples;;
 
-let tp : int tape = ([], 1, [2; 3]);;
-let tpId : (int, string) turingMachineID = (tp, "halt")
-let (newTp, state) = tpId
+let machine = Repeat_0_1.machine;;
 
-let () = print_endline state
+let id = Run.id_of_machine machine;;
+
+let rec print_step machine id acc = 
+  if (acc > 10) then () 
+  else 
+    match Run.step id machine with
+    | Some (new_tp, new_state) ->
+      let tp_in_string = Visualizer.string_of_tape 0 string_of_int new_tp in
+      print_endline tp_in_string;
+      print_endline new_state;
+      print_endline "";
+      print_step machine (new_tp, new_state) (acc + 1)
+    | None -> print_endline "HALT"
+;;
+
+let () = 
+  let (new_tp, new_state) = id in
+  let tp_in_string = Visualizer.string_of_tape 0 string_of_int new_tp in
+  print_endline tp_in_string;
+  print_endline new_state;
+  print_endline "\n";
+  print_step machine id 0
 ;;
 
