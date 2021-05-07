11f14aac4d81938facfdff8b421f8ba66e8b9f6d
Raul <raulrufino0@gmail.com>
2021-05-07 17:53:01 -0300

    feat(repeat_0_1): In this machine, add a counter to avoid infinite loop.

--- examples/repeat_0_1.ml
+++ examples/repeat_0_1.ml
@@ -1,17 +1,18 @@
 open TuringMachine
 
-let repeat_0_1: (int, string) Data.transition = 
+let repeat_0_1: (int, int * string) Data.transition = 
   fun _ state -> match state with
-  | "Write0" -> Some (0, "Write1", Tape.Right)
-  | "Write1" -> Some (1, "Write0", Tape.Right)
+  | (10, _) -> None
+  | (acc, "Write0") -> Some (0, (acc + 1, "Write1"), Tape.Right)
+  | (acc, "Write1") -> Some (1, (acc + 1, "Write0"), Tape.Right)
   | _ -> None
 ;;
 
-let machine: (int, string) Data.turingMachine =
+let machine: (int, int * string) Data.turingMachine =
   let blank = 0 in
   let input = [] in
   let end_states = [] in
-  let init_state = "Write0" in
+  let init_state = (0, "Write0") in
   let transition = repeat_0_1 in
   (blank, input, init_state, end_states, transition)
 ;;
