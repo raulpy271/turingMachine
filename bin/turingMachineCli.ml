69d096bec209020e99dc5ff8f23bfaa60e0c478e
Raul <raulrufino0@gmail.com>
2021-05-08 08:56:13 -0300

    refactor(bin): use the created funtions to run an example machine

--- bin/turingMachineCli.ml
+++ bin/turingMachineCli.ml
@@ -5,25 +5,16 @@ let machine = Repeat_0_1.machine;;
 
 let id = Run.id_of_machine machine;;
 
-let rec print_step machine id acc = 
-  if (acc > 10) then () 
-  else 
-    match Run.step id machine with
-    | Some (new_tp, new_state) ->
-      let tp_in_string = Visualizer.string_of_tape 0 string_of_int new_tp in
-      print_endline tp_in_string;
-      print_endline new_state;
-      print_endline "";
-      print_step machine (new_tp, new_state) (acc + 1)
-    | None -> print_endline "HALT"
+let print_step id _ =
+  let _ = Utils.clear_term () in
+  match id with
+  | Some (tp, (_, new_state)) ->
+    let tp_in_string = Visualizer.string_of_tape 0 string_of_int tp in
+    print_endline ("tape:    " ^ tp_in_string);
+    print_endline ("state:   " ^ new_state ^ "\n");
+    Unix.sleep 1
+  | None -> print_endline "HALT"
 ;;
 
-let () = 
-  let (new_tp, new_state) = id in
-  let tp_in_string = Visualizer.string_of_tape 0 string_of_int new_tp in
-  print_endline tp_in_string;
-  print_endline new_state;
-  print_endline "\n";
-  print_step machine id 0
-;;
+let () = Run.run_machine_with ~f:print_step ~machine;;
 
