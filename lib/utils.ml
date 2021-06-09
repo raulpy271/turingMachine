22b20ae93f98bb406904d3e038b03eff0372ed55
Raul <raulrufino0@gmail.com>
2021-06-09 20:50:21 -0300

    feat: Add utils function to fill or cut a list with elements
    This will be used to fill or cut a tape to fit in terminal screen.

--- lib/utils.ml
+++ lib/utils.ml
@@ -7,9 +7,26 @@ let put_last_in_front list =
   in put_last_in_front_tail_recursive list []
 ;;
 
+
 let clear_term () = 
   match Sys.os_type with
   | "Unix" -> Sys.command "clear"
   | _ -> Sys.command "cls"
 ;; 
 
+
+let is_even num = num mod 2 == 0;;
+
+
+let fill_list list_to_fill final_len empty_symbol = 
+  let len_of_list_to_fill = List.length list_to_fill in
+  if final_len > len_of_list_to_fill
+  then
+    let list_with_empty_symbols = 
+      List.init (final_len - len_of_list_to_fill) (fun _ -> empty_symbol) in
+    list_to_fill @ list_with_empty_symbols
+  else 
+    let list_cutted = 
+      List.filteri (fun i _ -> i < final_len) list_to_fill in
+    list_cutted
+;;
