e7500bdcc13a793486b21e3b79659b45ee38f4d8
Raul <raulrufino0@gmail.com>
2021-06-10 11:16:05 -0300

    feat: Add function to fill the left and right side of a tape

--- lib/utils.ml
+++ lib/utils.ml
@@ -18,7 +18,7 @@ let clear_term () =
 let is_even num = num mod 2 == 0;;
 
 
-let fill_list list_to_fill final_len empty_symbol = 
+let fill_list_in_right list_to_fill final_len empty_symbol = 
   let len_of_list_to_fill = List.length list_to_fill in
   if final_len > len_of_list_to_fill
   then
@@ -30,3 +30,36 @@ let fill_list list_to_fill final_len empty_symbol =
       List.filteri (fun i _ -> i < final_len) list_to_fill in
     list_cutted
 ;;
+
+
+let fill_list_in_left list_to_fill final_len empty_symbol = 
+  let len_of_list_to_fill = List.length list_to_fill in
+  if final_len > len_of_list_to_fill
+  then
+    let list_with_empty_symbols = 
+      List.init (final_len - len_of_list_to_fill) (fun _ -> empty_symbol) in
+    list_with_empty_symbols @ list_to_fill 
+  else 
+    let list_cutted = 
+      List.filteri 
+        (fun i _ -> i >= len_of_list_to_fill - final_len) list_to_fill in
+    list_cutted
+;;
+
+
+let get_len_of_higher_size l = 
+  List.fold_left (
+    fun current_higher str -> 
+      String.length str
+      |> max current_higher 
+  ) 0 l
+;;
+
+
+let get_len_of_higher_size_in_tape tape =
+  match tape with
+  | (left_side, head, right_side) -> 
+      get_len_of_higher_size ( left_side @ [head] @ right_side)
+;;
+
+
