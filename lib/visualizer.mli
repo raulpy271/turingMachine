dc0a46a8079f50975b0146e093ac634204c31fd6
Raul <raulrufino0@gmail.com>
2021-06-11 13:19:11 -0300

    feat: Add support to print the tape and the state

--- lib/visualizer.mli
+++ lib/visualizer.mli
@@ -4,16 +4,24 @@ type 'symbol string_of = 'symbol -> string
 This is the signature of functions that convert elements of a tape to string.
  *)
 
-val string_of_tape : 
-  'symbol ->  'symbol string_of -> 'symbol Data.tape -> string
+
+val view_of_machine :
+  ('symbol, 'state) Data.turingMachineID ->
+  'symbol ->
+  'symbol string_of ->
+  'state string_of -> string
 (**
 This function will convert a tape to string. It will be usefull to show a tape in terminal.
 
-@param empty This first parameter is the empty symbol of the tape.
+@param id The ID of the machine in an instant.
+
+@param empty this is the empty symbol.
 
 @param string_of_symbol This is a function that will convert the elements of the tape to string.
 
-@param tape This is the tape.
+@param string_of_state This is a function that will convert a state to a string.
+
+@return The string representing the machine, this string will be printed in the screen.
  *)
 
 
