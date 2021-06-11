72688b852bfa0a73282dd0494ced44b207a964a6
Raul <raulrufino0@gmail.com>
2021-06-11 13:05:25 -0300

    refact: the function `step` in the module run
    This function only does a step in a machine, now it does a step and calls a function passed through your arguments. This function can be used to print each step of the execution.

--- lib/run.mli
+++ lib/run.mli
@@ -22,6 +22,10 @@ and create a tape from these arguments.
 
 val step : ('symbol, 'state) Data.turingMachineID ->
   ('symbol, 'state) Data.turingMachine ->
+  ( 
+    ('symbol, 'state) Data.turingMachineID option ->
+    ('symbol, 'state) Data.turingMachine -> unit
+  ) ->
   ('symbol, 'state) Data.turingMachineID option
 (**
 This function aims to represent the behavior of a given machine. So, it gives each step of the machine.
@@ -29,6 +33,8 @@ This function aims to represent the behavior of a given machine. So, it gives ea
 @param id This is the instantaneous description of a machine.
 
 @param machine This is the machine which the id represents.
+
+@param f This is a function that will be called in each call of [ step ], it can be used to show the machine in terminal in every step of the execution.
 
 @return The return is the next ID of the machine, this ID is given by the transition function. Besides, the returned value is inside a [ option ] because when the machine halt in the next step, it return [ None ].
 *)
@@ -47,7 +53,7 @@ val run_machine_with :
 (**
 This function run the machine until it halt. In each step of the executation,
 it's call the function [ f ] with the ID and the machine.
-An example of usage, is to print each step of the executation, whih is implemented in {{:https://github.com/raulpy271/turingMachine/blob/main/bin/turingMachineCli.ml} turingMachineCli} module.
+An example of usage, is to print each step of the executation, which is implemented in {{:https://github.com/raulpy271/turingMachine/blob/main/bin/turingMachineCli.ml} turingMachineCli} module.
 
 @param f This function do something in each step of the machine, It's should return an [ unit ] value, and the ID is inside an [ option ] type, so, if the value is [ None ] the machine has halted.
 
