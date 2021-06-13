a9699057d5b9b507deb62d71c36d0e1d8348033d
Raul <raulrufino0@gmail.com>
2021-06-13 09:44:49 -0300

    refator: Add final struture of the main module

--- bin/turingMachineCli.ml
+++ bin/turingMachineCli.ml
@@ -1,18 +1,33 @@
 open TuringMachine;;
 open Examples;;
 
-let machine = Repeat_0_1.machine;;
 
-let print_step id _ =
+module Machine = struct
+  include Write_msg
+end;;
+
+
+let machine          = Machine.machine;;
+let string_of_state  = Machine.string_of_state;;
+let string_of_symbol = Machine.string_of_symbol;;
+
+
+let print_step id machine =
   let _ = Utils.clear_term () in
+  let (empty, _, _, _, _) = machine in
   match id with
-  | Some (tp, (_, new_state)) ->
-    let tp_in_string = Visualizer.string_of_tape 0 string_of_int tp in
-    print_endline ("tape:    " ^ tp_in_string);
-    print_endline ("state:   " ^ new_state ^ "\n");
-    Unix.sleep 1
+  | Some id_without_option ->
+    let machine_in_string = 
+      Visualizer.view_of_machine
+        id_without_option empty string_of_symbol string_of_state in
+    print_endline machine_in_string;
+    Unix.sleepf 0.3
   | None -> print_endline "HALT"
 ;;
 
-let () = Run.run_machine_with ~f:print_step ~machine;;
+
+let () = 
+  Run.run_machine_with ~f:print_step ~machine
+;;
+
 
