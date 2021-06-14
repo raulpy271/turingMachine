1d0be1a68594e07619c2885bfa07c4e2e450e567
Raul <raulrufino0@gmail.com>
2021-06-14 10:39:44 -0300

    fix: Don't clear the screen after the machine has halted
    So, the user can see the state before the machine halt

--- bin/turingMachineCli.ml
+++ bin/turingMachineCli.ml
@@ -13,20 +13,21 @@ let string_of_symbol = Machine.string_of_symbol;;
 
 
 let print_step id machine =
-  let _ = Utils.clear_term () in
   let (empty, _, _, _, _) = machine in
   match id with
   | Some id_without_option ->
     let machine_in_string = 
       Visualizer.view_of_machine
         id_without_option empty string_of_symbol string_of_state in
+    let _ = Utils.clear_term () in
     print_endline machine_in_string;
     Unix.sleepf 0.3
-  | None -> print_endline "HALT"
+  | None -> ()
 ;;
 
 
-let () = 
+let () =
+  let _ = Utils.clear_term () in
   Run.run_machine_with ~f:print_step ~machine
 ;;
 
